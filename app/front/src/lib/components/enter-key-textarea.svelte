e34f2298f4fe4151da407de8ac3f489fc3e3d27f
shunito <itohshun314@gmail.com>
2023-12-04 02:12:27 +0900

    fix style

--- app/front/src/lib/components/enter-key-textarea.svelte
+++ app/front/src/lib/components/enter-key-textarea.svelte
@@ -55,9 +55,26 @@
       background-color: unset;
       margin: unset;
       padding: unset;
+      padding-top: 1ex;
+      padding-bottom: 1ex;
       border: unset;
       width: 100%;
       background-color: lavenderblush;
+      font-style: unset;
+      font-variant-ligatures: unset;
+      font-variant-caps: unset;
+      font-variant-numeric: unset;
+      font-variant-east-asian: unset;
+      font-variant-alternates: unset;
+      font-variant-position: unset;
+      font-weight: unset;
+      font-stretch: unset;
+      font-size: unset;
+      font-family: unset;
+      font-optical-sizing: unset;
+      font-kerning: unset;
+      font-feature-settings: unset;
+      font-variation-settings: unset;
     }
   }
 </style>
