c70818439024621fc368327a0113b284b9c9c55f
shunito <itohshun314@gmail.com>
2023-12-08 18:22:41 +0900

    show logs

--- app/front/src/routes/home/+page.svelte
+++ app/front/src/routes/home/+page.svelte
@@ -2,6 +2,7 @@
   import type { IUserWithRelationship } from "$lib/back/user/user.entity";
   import type { PageData } from "./$types";
   import SetRelationshipButtons from "$lib/components/set-relationship-buttons.svelte";
+  import { showTimeDiff } from "$lib/time-helper";
 
   export let data: PageData;
   let isMatching = false;
@@ -56,6 +57,8 @@
   function callbackInvalidate() {
     user_name = "";
   }
+
+  const now = Math.floor(Date.now() / 1000);
 </script>
 
 <svelte:head>
@@ -73,29 +76,45 @@
     <button on:click={startMatchmaking}>マッチメイキングを開始</button>
   {/if}
 
-  <search>
-    <label>
-      Search by user name:
-      <input type="search" bind:value={user_name} />
-    </label>
-  </search>
-  <ul>
-    {#await users_promise then users}
-      {#each users as user}
-        {#if user.relationship !== -1}
-          <li>
-            <a href="/user/{user.id}">{user.displayName}</a>
-            <a href="/home/direct-message/{user.id}"> Send Direct Message </a>
-            <SetRelationshipButtons
-              user_id={user.id}
-              user_relationship={user.relationship}
-              callback={callbackInvalidate}
-            />
-          </li>
-        {/if}
-      {/each}
-    {/await}
-  </ul>
+  <div>
+    <search>
+      <label>
+        Search by user name:
+        <input type="search" bind:value={user_name} />
+      </label>
+    </search>
+    <ul>
+      {#await users_promise then users}
+        {#each users as user}
+          {#if user.relationship !== -1}
+            <li>
+              <a href="/user/{user.id}">{user.displayName}</a>
+              <a href="/home/direct-message/{user.id}"> Send Direct Message </a>
+              <SetRelationshipButtons
+                user_id={user.id}
+                user_relationship={user.relationship}
+                callback={callbackInvalidate}
+              />
+            </li>
+          {/if}
+        {/each}
+      {/await}
+    </ul>
+  </div>
+  <div>
+    <h2>History - Win: {data.win} Lose: {data.lose}</h2>
+    {#each data.logs as log}
+      <div class="log-{log.win ? 'win' : 'lose'}">
+        <p>
+          Result: {log.win ? "win" : "lose"}
+          {"("}<time title={new Date(log.date * 1000).toLocaleString()}>
+            {showTimeDiff(log.date, now)}
+          </time>{")"}
+        </p>
+        <p><a href="/user/{log.id.toString()}">Opponent: {log.name}</a></p>
+      </div>
+    {/each}
+  </div>
 </main>
 
 <style>
@@ -115,6 +134,26 @@
       width: 100%;
       text-align: center;
       background-color: antiquewhite;
+    }
+  }
+
+  .log-win {
+    background-color: aqua;
+    padding: 1ex;
+
+    & a {
+      text-decoration: none;
+    }
+  }
+
+  .log-lose {
+    background-color: indigo;
+    color: white;
+    padding: 1ex;
+
+    & a {
+      text-decoration: none;
+      color: white;
     }
   }
 </style>
